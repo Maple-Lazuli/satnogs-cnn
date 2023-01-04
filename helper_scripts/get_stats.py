fcdc5a5d341580e9ea38d581d7ec5ebbf53ef6d1
Ada <the.nostra.tymus@gmail.com>
2023-01-03 19:50:38 -0500

    added dask for creating the statistics

--- helper_scripts/get_stats.py
+++ helper_scripts/get_stats.py
@@ -1,25 +1,29 @@
-import pandas as pd
+import dask.dataframe as dd
+from dask.diagnostics import ProgressBar
 import numpy as np
 import os
 import json
 import argparse
 
+
 def load_waterfall(x):
     return np.fromfile(x, dtype=np.uint8).reshape(-1, 623)
 
-def main(flags):
 
+def main(flags):
     if not os.path.isfile(flags.csv):
         print(f'could not find {flags.csv}')
         exit()
 
-    train = pd.read_csv(flags.csv)
-    train['loaded_waterfall'] = train['waterfall_location'].apply(lambda x: load_waterfall(x))
-    train['waterfall_sum'] = train['loaded_waterfall'].apply(lambda x: np.sum(x))
-    mu = sum(train['waterfall_sum']) / (623 * 1542 * train.shape[0])
+    train = dd.read_csv(flags.csv)
+    train['loaded_waterfall'] = train['waterfall_location'].apply(lambda x: load_waterfall(x),
+                                                                  meta=('loaded_waterfall', np.uint8))
+    train['waterfall_sum'] = train['loaded_waterfall'].apply(lambda x: np.sum(x, dtype=int),
+                                                             meta=('waterfall_sum', int))
+    with ProgressBar():
+        train = train.compute()
 
-    train['loaded_waterfall'] = train['loaded_waterfall'].apply(lambda x: (x - mu) ** 2)
-    train['waterfall_sum'] = train['loaded_waterfall'].apply(lambda x: np.sum(x))
+    mu = sum(train['waterfall_sum']) / (623 * 1542 * train.shape[0])
     sigma = sum(train['waterfall_sum']) / (623 * 1542 * train.shape[0] - 1) ** .5
 
     # Save Mu and Sigma
@@ -29,8 +33,8 @@ def main(flags):
             'sigma': sigma
         }, file_out)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--csv', type=str,
